ba3d444288532b486fc0d53bb43e8b6362f42d61
Kento.Yamada <ymd65536@outlook.com>
2025-08-01 10:28:20 +0000

    feat(devcontainer): add postCreateCommand to devcontainer configuration

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,7 @@
+# bin/bash
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y python3 python3-pip
+export STAGE=dev
+pip config set install.user 'false'
+pip config set global.index-url https://pypi.org/simple
