ee47408566fd54093a6acfa1ebd13b230578807a
Kento.Yamada <ymd65536@outlook.com>
2025-08-01 10:40:57 +0000

    feat(cdk): add Lambda function and layer for vector operations

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,9 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vectorsLayer = new lambda.LayerVersion(this, 'VectorsLayer',{
+      code: lambda.Code.fromAsset('lambda/layers'),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_12],
+      description: 'Layer for vector operations',
+      layerVersionName: 'VectorsLayer',
+    });
+
+    const createIndexFunction = new lambda.Function(this, 'CreateIndexFunction', {
+      runtime: lambda.Runtime.PYTHON_3_12,
+      code: lambda.Code.fromAsset('lambda/function'),
+      handler: 'lambda_function.lambda_handler',
+      layers: [vectorsLayer],
+      environment: {
+        VECTOR_ENGINE_BUCKET: 'your-bucket-name', // Replace with your actual bucket name
+      },
+      timeout: cdk.Duration.minutes(5),
+      memorySize: 512,
+    });
 
   }
 }
